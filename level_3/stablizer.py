339d64dc425d2d28c21f0671a693e9edc01b9931
Hook12aaa <28310645+Hook12aaa@users.noreply.github.com>
2021-04-13 15:51:32 +0100

    deleate mp4

--- level_3/stablizer.py
+++ level_3/stablizer.py
@@ -1,28 +1,59 @@
 import cv2.cv2 as cv2
 import numpy as np
 
-cap = cv2.VideoCapture("./footage/drift_shakey_pratice.mp4")
 
-
-class drift_remove():
-    def __init__(self):
+class stabilize():
+    def __init__(self,force_camera = False,Camera_nunber = 0):
         self.previous_frame = 0
         self.frame_count = 0
         self.buffer_of_x = []
+        self.__activiate_camera(force_camera,Camera_nunber)
 
-    def __remove_drift(self):
-        """This gets a dictation of the drone by reading the buffer of past recordings """
-        total = sum(self.buffer_of_x) / 10
+
+
+    def __activiate_camera(self,user_force_camera:bool,num:int):
+        """private function that forces camera to speed up loading time
+
+        Args:
+            num (int): Camera's Device numb eg 1 or 2....
+            user_force_camera (bool): True at default will open camera on auto
+        """
+        if not user_force_camera:
+            for i in range(num):
+                self.cap = cv2.VideoCapture(i)
+                if self.cap is not None and self.cap.isOpened():
+                    break
+                else:
+                    continue
+    
+        if  user_force_camera:
+            self.cap = cv2.VideoCapture(num)
+
+    def get_video_frame(self) ->np.array:
+        """parse frame for camera into a return. Gets a frame
+
+        Returns:
+            np.array: your image from the camera
+        """
+        __ , self.frame = self.cap.read()
+        return self.frame
+
+    def __get_total(self):
+        """Sums up movement in a range of frames of the video to find the avarage
+
+        Returns:
+            float: the total possible shake to happen
+        """
+        total = round(sum(self.buffer_of_x) / 10,1)
         self.buffer_of_x = []
-        if total > 0:
-            return "LEFT", total
-        if total < 0:
-            return "RIGHT", total * -1
-        if total == 0:
-            return "STRAIGHT", total
+        return total 
 
     def __catch_x(self, grey_img):
-        """Takes (x,_) of a sharp point and adds it to an array"""
+        """ Catch the x cord from the frame of the video. Will use goodFeaturesToTrack
+
+        Args:
+            grey_img (np.array): The Grey Scale that you would like to track
+        """
         corners = cv2.goodFeaturesToTrack(grey_img, 1, 0.01, 10)
         x, y = np.int0(corners).ravel()
         total = (x - self.previous_frame)
@@ -31,12 +62,39 @@ class drift_remove():
         self.previous_frame = x
         self.frame_count += 1
 
-    def stabilize_camera(self, grey_img):
-        """frame needed for processing. returns every 20 iteration"""
-        if self.frame_count == 20:
+    def __get_metrics(self, grey_img):
+        """frame needed for processing. returns every 20 iteration
+
+        Args:
+            grey_img (np.array) [description]
+
+        Returns:
+            float: The shake amount dectected in the image
+        """
+        if self.frame_count == 10:
             self.frame_count = 0
-            return self.__remove_drift()
+            return self.__get_total()
         else:
             self.__catch_x(grey_img)
-            return False
+
+
+
+    def show_image(self,frame= None) -> None:
+        """will display image as a spearte window when called
+
+        Args:
+            frame (OpenCV_image): Can be empty if needed, just show passing the frame into another area
+        """
+        cv2.imshow('frame', self.frame)
+
+
+    def get_shake(self,grey:np.array) ->None:
+        """get shake value and will print it out
+
+        Args:
+            grey (np.array): the grey of the video
+        """
+        r = self.__get_metrics(grey)
+        if r != None:
+            print(r)
 
